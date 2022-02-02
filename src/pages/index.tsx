b9a434a33d266fa5f142a986d72a0103f0dfdcd9
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-02-02 12:52:43 -0300

    chore: WIP page Home episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,23 @@
 import Head from "next/head";
+import { GetStaticProps } from "next";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { api } from "../services/api";
+import { convertDurationTime } from "../utils/convertDurationTime";
 
-export default function Home() {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+  //...
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
   return (
     <>
       <Head>
@@ -8,17 +25,39 @@ export default function Home() {
       </Head>
 
       <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
     </>
   );
 }
 
-export const getStaticProps = async () => {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      thumbnail: episode.thumbnail,
+      description: episode.description,
+      url: episode.file.url,
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationTime(Number(episode.file.duration)),
+    };
+  });
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   };
